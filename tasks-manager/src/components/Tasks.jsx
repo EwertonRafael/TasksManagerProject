a4abcd1dddf29bad362a02b1de3493721ad0516d
Ewerton Rafael <ton_rafael@hotmail.com>
2025-02-27 05:46:31 -0300

    refactor: Task component

--- tasks-manager/src/components/Tasks.jsx
+++ tasks-manager/src/components/Tasks.jsx
@@ -1,7 +1,19 @@
-const Tasks = () => {
+import { IoChevronForwardOutline, IoTrashOutline   } from "react-icons/io5";
+import { GiCheckMark } from "react-icons/gi";
+
+const Tasks = ({tasks, onTaskClick, onDeleteTask}) => {
   return (
     <>
-      <h1>tarefa1</h1>
+      <h1>{tasks.map(task => <li key={task.id}>
+        <button onClick={() => onTaskClick(task.id)}>{task.title}
+          {task.isCompleted && <span><GiCheckMark /></span>}
+        </button>
+
+        <button><IoChevronForwardOutline /></button>
+
+        <button onClick={() => onDeleteTask(task.id)}><IoTrashOutline/> </button>
+      </li>)}
+      </h1>
     </>
   )
 }
